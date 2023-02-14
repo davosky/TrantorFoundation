10108e78f2cfa159df0bc18379de13e2379c7d4d
Davo Davosky <davodavosky@gmail.com>
2023-02-14 16:50:35 +0100

    Bugfixed Abilities

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -20,19 +20,19 @@ class Ability
       # Can manage all owned records on Refund model
       can :create, Refund
       can :update, Refund do |refund|
-        refund.user == user
+        refund.user_id == user.id
       end
       can :destroy, Refund do |refund|
-        refund.user == user
+        refund.user_id == user.id
       end
       can :read, Refund do |refund|
-        refund.user == user
+        refund.user_id == user.id
       end
       can :print, Refund do |refund|
-        refund.user == user
+        refund.user_id == user.id
       end
       can :print_list, Refund do |refund|
-        refund.user == user
+        refund.user_id == user.id
       end
       # Can manage all owned records on RefundClosure model
       can :create, RefundClosure
@@ -40,7 +40,7 @@ class Ability
         refund_closure.user.province == user.province && refund_closure.user.region == user.region
       end
       can :destroy, RefundClosure do |refund_closure|
-        refund_closure.user == user if refund_closure.payed != true
+        refund_closure.user_id == user.id if refund_closure.payed != true
       end
       can :read, RefundClosure do |refund_closure|
         refund_closure.user.province == user.province && refund_closure.user.region == user.region
@@ -92,29 +92,17 @@ class Ability
         disease.user.province == user.province && disease.user.region == user.region
       end
       # Can manage all owned records on Place model
-      can :manage, Place do |place|
-        place.user == user
-      end
+      can :manage, Place, user_id: user.id
       # Can manage all owned records on Reason model
-      can :manage, Reason do |reason|
-        reason.user == user
-      end
+      can :manage, Reason, user_id: user.id
       # Can manage all owned records on Road model
-      can :manage, Road do |road|
-        road.user == user
-      end
+      can :manage, Road, user_id: user.id
       # Can manage all owned records on Structure model
-      can :manage, Structure do |structure|
-        structure.user == user
-      end
+      can :manage, Structure, user_id: user.id
       # Can manage all owned records on Transport model
-      can :manage, Transport do |transport|
-        transport.user == user
-      end
+      can :manage, Transport, user_id: user.id
       # Can manage all owned records on Veichle model
-      can :manage, Veichle do |veichle|
-        veichle.user == user
-      end
+      can :manage, Veichle, user_id: user.id
     end
     # ==================================================================
     # *** Manager ***
@@ -126,110 +114,98 @@ class Ability
     if user.regular == true
       # Can manage all owned records on Refund model
       can :read, Refund do |refund|
-        refund.user == user
+        refund.user_id == user.id
       end
       can :create, Refund
       can :update, Refund do |refund|
-        refund.user == user
+        refund.user_id == user.id
       end
       can :destroy, Refund do |refund|
-        refund.user == user
+        refund.user_id == user.id
       end
       can :read, Refund do |refund|
-        refund.user == user
+        refund.user_id == user.id
       end
       can :print, Refund do |refund|
-        refund.user == user
+        refund.user_id == user.id
       end
       can :print_list, Refund do |refund|
-        refund.user == user
+        refund.user_id == user.id
       end
       # Can manage all owned records on RefundClosure model
       can :read, RefundClosure do |refund_closure|
-        refund_closure.user == user
+        refund_closure.user_id == user.id
       end
       can :create, RefundClosure
       can :update, RefundClosure do |refund_closure|
-        refund_closure.user == user
+        refund_closure.user_id == user.id
       end
       can :destroy, RefundClosure do |refund_closure|
-        refund_closure.user == user && refund_closure.payed != true
+        refund_closure.user_id == user.id && refund_closure.payed != true
       end
       can :read, RefundClosure do |refund_closure|
-        refund_closure.user == user
+        refund_closure.user_id == user.id
       end
       # Can manage all owned records on Holiday model
       can :read, Holiday do |holiday|
-        holiday.user == user
+        holiday.user_id == user.id
       end
       can :create, Holiday
       can :update, Holiday do |holiday|
-        holiday.user == user && holiday.processed != true
+        holiday.user_id == user.id && holiday.processed != true
       end
       can :destroy, Holiday do |holiday|
-        holiday.user == user && holiday.processed != true
+        holiday.user_id == user.id && holiday.processed != true
       end
       can :read, Holiday do |holiday|
-        holiday.user == user
+        holiday.user_id == user.id
       end
       can :search, Holiday do |holiday|
-        holiday.user == user
+        holiday.user_id == user.id
       end
       # Can manage all owned records on HourlyHoliday model
       can :read, HourlyHoliday do |hourly_holiday|
-        hourly_holiday.user == user
+        hourly_holiday.user_id == user.id
       end
       can :create, HourlyHoliday
       can :update, HourlyHoliday do |hourly_holiday|
-        hourly_holiday.user == user && hourly_holiday.processed != true
+        hourly_holiday.user_id == user.id && hourly_holiday.processed != true
       end
       can :destroy, HourlyHoliday do |hourly_holiday|
-        hourly_holiday.user == user && hourly_holiday.processed != true
+        hourly_holiday.user_id == user.id && hourly_holiday.processed != true
       end
       can :search, HourlyHoliday do |hourly_holiday|
-        hourly_holiday.user == user
+        hourly_holiday.user_id == user.id
       end
       # Can manage all owned records on Disease model
       can :read, Disease do |disease|
-        disease.user == user
+        disease.user_id == user.id
       end
       can :create, Disease
       can :update, Disease do |disease|
-        disease.user == user && disease.processed != true
+        disease.user_id == user.id && disease.processed != true
       end
       can :destroy, Disease do |disease|
-        disease.user == user && disease.processed != true
+        disease.user_id == user.id && disease.processed != true
       end
       can :read, Disease do |disease|
-        disease.user == user
+        disease.user_id == user.id
       end
       can :search, Disease do |disease|
-        disease.user == user
+        disease.user_id == user.id
       end
       # Can manage all owned records on Place model
-      can :manage, Place do |place|
-        place.user == user
-      end
+      can :manage, Place, user_id: user.id
       # Can manage all owned records on Reason model
-      can :manage, Reason do |reason|
-        reason.user == user
-      end
+      can :manage, Reason, user_id: user.id
       # Can manage all owned records on Road model
-      can :manage, Road do |road|
-        road.user == user
-      end
+      can :manage, Road, user_id: user.id
       # Can manage all owned records on Structure model
-      can :manage, Structure do |structure|
-        structure.user == user
-      end
+      can :manage, Structure, user_id: user.id
       # Can manage all owned records on Transport model
-      can :manage, Transport do |transport|
-        transport.user == user
-      end
+      can :manage, Transport, user_id: user.id
       # Can manage all owned records on Veichle model
-      can :manage, Veichle do |veichle|
-        veichle.user == user
-      end
+      can :manage, Veichle, user_id: user.id
     end
     # ==================================================================
     # *** Regular User ***
