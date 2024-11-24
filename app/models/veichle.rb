b0cad3301ec31fdeb4dff4f4d71995424d777b5f
Davo Davosky <davodavosky@gmail.com>
2024-11-24 11:01:19 +0100

    Revamped Modules

--- app/models/veichle.rb
+++ app/models/veichle.rb
@@ -2,5 +2,5 @@ class Veichle < ApplicationRecord
   belongs_to :user, optional: true
 
   validates :license_plate, uniqueness: true
-  validates :license_plate, :name, :producer, :position, presence: true
+  validates :license_plate, :name, :producer, :position, :cost_per_km, presence: true
 end
