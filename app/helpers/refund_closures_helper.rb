ec4ea49d7b3381e870507e880d4c11ce1d286703
Davo Davosky <davodavosky@gmail.com>
2024-12-10 08:25:47 +0100

    Reintroduced payed non payed on refund_closures search view

--- app/helpers/refund_closures_helper.rb
+++ app/helpers/refund_closures_helper.rb
@@ -1,9 +1,9 @@
 module RefundClosuresHelper
   def payed_fg?(refund_closure)
     if refund_closure.payed == true
-      "fg-payed"
+      "<span class='text-success'><i class='fa-solid fa-thumbs-up'></i>&nbsp;<i class='fa-solid fa-cash-register'></i>&nbsp;Pagato</span>".html_safe
     else
-      "fg-unpayed"
+      "<span class='text-warning'><i class='fa-solid fa-thumbs-down'></i>&nbsp;<i class='fa-solid fa-cash-register'></i>&nbsp;DaPagare</span>".html_safe
     end
   end
 end
